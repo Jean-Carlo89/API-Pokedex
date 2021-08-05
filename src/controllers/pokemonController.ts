cc069bb6e7e879848c21a50b77fd468cf4583d9e
Jean Carlo <jeancarlomo@hotmail.com>
2021-08-05 20:56:55 -0300

    saving pokemons with succes

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -1,5 +1,41 @@
 import { Request, Response } from "express";
+import {getRepository} from "typeorm"
+import Pokemon from '../entities/Pokemon'
+
+interface newPokemon{
+    
+    id:number;
+
+    
+    name: string;
+
+   
+    number: number;
+
+   
+    image:string;
+
+   
+    weight:number;
+
+
+   
+    height:number;
+
+
+   
+    baseExp:number;
+}
+
+
 
 export async function insert(req: Request, res: Response){
-    
+   console.log(req.body)
+   
+   for(let i=0;i<req.body.length; i++){
+
+    await getRepository(Pokemon).insert(req.body[i])
+   }
+   
+   
 }
