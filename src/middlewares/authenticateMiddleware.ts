d5410b19724c47481477818dd3d3aa70758c7073
Jean Carlo <jeancarlomo@hotmail.com>
2021-08-23 09:43:44 -0300

    adding try catch in middleware verification

--- src/middlewares/authenticateMiddleware.ts
+++ src/middlewares/authenticateMiddleware.ts
@@ -3,15 +3,20 @@ import * as userService from '../services/userService'
 
 
 export async function authenticateMiddleware(req:Request,res:Response,next:NextFunction){
-    
-    const header = req.headers["authorization"]
-    const token = header.split("Bearer ")[1]
-   const user = await userService.authenticate(token)
-    
-   if(user===null){
-    return res.sendStatus(401)
-   }
-
-   res.locals.user=user
-    next()
+    try{
+      const header = req.headers["authorization"]
+      const token = header.split("Bearer ")[1]
+     const user = await userService.authenticate(token)
+      
+     if(user===null){
+      return res.sendStatus(401)
+     }
+  
+     res.locals.user=user
+      next()
+    }catch(e){
+      
+    return res.sendStatus(401);
+    }
+   
   }
